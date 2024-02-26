f9e4408cba36f2fa64fab029a724593888cfc662
drezi29 <julia.drezek98@gmail.com>
2024-02-26 21:30:35 +0100

    tictactoe: player, action and result methods' implementation

--- search/tictactoe/tictactoe.py
+++ search/tictactoe/tictactoe.py
@@ -2,7 +2,7 @@
 Tic Tac Toe Player
 """
 
-import math
+import copy, math
 
 X = "X"
 O = "O"
@@ -22,21 +22,34 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    if [item for row in board for item in row].count(EMPTY) % 2 == 0:
+        return O
+    return X
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    actions_set = set()
+    for x in range(3):
+        for y in range(3):
+            if board[x][y] == EMPTY:
+                actions_set.add((x,y))
+    return actions_set
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    (x, y) = action
+    if board[x][y] != EMPTY:
+        raise Exception('Action forbidden - that cell is not empty!')
+    
+    board_copy = copy.deepcopy(board)
+    board_copy[x][y] = player(board_copy)
+    return board_copy
 
 
 def winner(board):
