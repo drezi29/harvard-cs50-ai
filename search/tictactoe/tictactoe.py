7e51fd5b24790a5341b5b6ad2fec95a1d7d3e484
drezi29 <julia.drezek98@gmail.com>
2024-02-27 21:39:41 +0100

    tictactoe: terminal and utility method implementation

--- search/tictactoe/tictactoe.py
+++ search/tictactoe/tictactoe.py
@@ -8,6 +8,16 @@ X = "X"
 O = "O"
 EMPTY = None
 
+WINNING_STATES = [
+    [(0,0), (0,1), (0,2)],
+    [(1,0), (1,1), (1,2)],
+    [(2,0), (2,1), (2,2)],
+    [(0,0), (1,0), (2,0)],
+    [(0,1), (1,1), (2,1)],
+    [(0,2), (1,2), (2,2)],
+    [(0,0), (1,1), (2,2)],
+    [(0,2), (1,1), (2,0)]
+]
 
 def initial_state():
     """
@@ -56,21 +66,41 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    for state in WINNING_STATES:
+        values = []
+        for position in state:
+            (x,y) = position
+            values.append(board[x][y])
+        if values.count(values[0]) == len(values) and values[0] != EMPTY:
+            return values[0]
+    return None
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    empty_fields = [item for row in board for item in row].count(EMPTY)
+    if empty_fields > 4:
+        return False
+    
+    if winner(board) != None or empty_fields == 0:
+        return True
+    else:
+        return False
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    game_winner = winner(board)
+    if game_winner == X:
+        return 1
+    elif game_winner == O:
+        return -1
+    else:
+        return 0
 
 
 def minimax(board):
